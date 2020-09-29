a876eea9117b6059dff5f97336c6ad5bc4471a58
wynterwolf <shinyguns@gmail.com>
2020-09-29 14:01:23 -0500

    change table

--- db/migrate/20200928233245_create_songs.rb
+++ db/migrate/20200928233245_create_songs.rb
@@ -2,8 +2,8 @@ class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :name
-      t.integer :artist_id
-      t.integer :genre_id
+      t.belongs_to :artist
+      t.belongs_to :genre
 
       t.timestamps
     end
