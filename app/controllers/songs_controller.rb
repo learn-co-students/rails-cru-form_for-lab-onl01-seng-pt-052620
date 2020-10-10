20856002792f7c7df509a6663a290504039291ad
M. Gage Morgan <gage@gages.blog>
2020-10-09 20:06:24 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,29 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find_by(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new
+    @song = Song.create(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+
+  def edit
+    @song = Song.find_by(params[:id])
+  end
+
+  def update
+    @song = Song.find_by(params[:id])
+    @song = Song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
+  end
+end
